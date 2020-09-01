461b25b1957afad0d2f2fa77fbabb65f88a1ba1d
529038378 <529038378@qq.com>
2020-09-01 13:51:08 +0800

    加入spdlog

---
+++ Modules/Logger/LoggerWrapper.cpp
@@ -0,0 +1,35 @@
+#include "LoggerWrapper.h"
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/basic_file_sink.h"
+
+LoggerWrapper* LoggerWrapper::Instance()
+{
+	if (NULL == m_instance.get())
+	{
+		m_instance = std::make_unique<LoggerWrapper>();
+	}
+	return m_instance.get();
+}
+
+std::unique_ptr<LoggerWrapper> LoggerWrapper::m_instance;
+
+void LoggerWrapper::Init()
+{
+#ifdef __Debug
+	spdlog::set_level(spdlog::level::debug);
+#else
+#endif
+	//注册不同的log方式
+	auto console = spdlog::stdout_color_mt("console");
+	auto out_error = spdlog::stderr_color_mt("stderr");
+
+	auto file_logger = spdlog::basic_logger_mt("file_logger", "logs.txt");
+	spdlog::set_default_logger(file_logger);
+}
+
+LoggerWrapper::LoggerWrapper()
+{
+	Init();
+}
+
